0f7ee290d147d58ba259c5d1137a5ea8074d49eb
--replace-all <nathanbowness@gmail.com>
2017-09-01 14:31:13 -0400

    Updated the install script to take away the cd to app, and changed references to python3.

--- install genesippr portal.sh
+++ install genesippr portal.sh
@@ -24,13 +24,13 @@ source ~/genesipprportal/bin/activate
 pip install requirements.txt
 
 echo "Migrating Database If Needed"
-cd /app/ && python manage.py makemigrations
-cd /app/ && python manage.py migrate
+python3 manage.py makemigrations
+python3 manage.py migrate
 
 echo "Setting Up Django"
 echo "from django.contrib.auth.models import User; User.objects.filter(email='admin@example.com').delete(); User.objects.create_superuser('admin', 'admin@example.com', 'admin')" | python manage.py shell
-cd /app/ && python manage.py runserver 0.0.0.0:8000 &
+python3 manage.py runserver 0.0.0.0:8000
 
 echo "Setting Up Celery"
 # Starts Celery and workers given 24 hours max to complete a task and output logs
-cd /app/ && python manage.py celery worker --time-limit=86400 --loglevel=info 
+python3 manage.py celery worker --time-limit=86400 --loglevel=info
